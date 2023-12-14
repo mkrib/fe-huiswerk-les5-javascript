f277654a9aae05726e5d3cdec3a029f79dc51e04
mkrib <mkribben@mac.com>
2023-12-14 22:04:19 +0100

    invullingen tm opdracht 3

--- scripts/main.js
+++ scripts/main.js
@@ -71,4 +71,56 @@ const departments = {
     }
 }
 
+// Opdracht 1
 console.log(departments);
+console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers");
+console.log("Marketing is een leuke afdeling om te werken. " + departments.marketing.description);
+console.log(("De afdeling Customer Service heeft " + departments['customer-service'].numberOfEmployees) + " medewerkers");
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[1].description);
+
+// Opdracht 2
+const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]");
+console.log(userInput);
+if (userInput === "marketing") {
+    console.log("Je koos " + userInput + ". " + departments.marketing.description)
+}
+else if (userInput === "sales") {
+    console.log("Je koos " + userInput + ". " + departments.sales.description)
+}
+else if (userInput === "customer-service") {
+    console.log("Je koos " + userInput + ". " + departments['customer-service'].description);
+}
+else {
+    console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.");
+}
+
+// Opdracht 3
+// const userInputDepartment = prompt("Je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.\n" +
+//     "0: " + departments.marketing.jobs[0].title + ",\n" +
+//     "1: " + departments.marketing.jobs[1].title + ",\n" +
+//     "2: " + departments.marketing.jobs[2].title + ",\n" +
+//     "3: " + departments.marketing.jobs[3].title + ",\n");
+// console.log(userInputDepartment);
+// if (userInputDepartment === "0") {
+//     console.log("Je koos " + departments.marketing.jobs[0].title + ". Een uitdagende rol! " + departments.marketing.jobs[0].description);
+// }
+// else if (userInputDepartment === "1") {
+//     console.log("Je koos " + departments.marketing.jobs[1].title + ". Een uitdagende rol! " + departments.marketing.jobs[1].description);
+// }
+// else if (userInputDepartment === "2") {
+//     console.log("Je koos " + departments.marketing.jobs[2].title + ". Een uitdagende rol! " + departments.marketing.jobs[2].description);
+// }
+// else if (userInputDepartment === "3") {
+//     console.log("Je koos " + departments.marketing.jobs[3].title + ". Een uitdagende rol! " + departments.marketing.jobs[3].description);
+// }
+// else {
+//     console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.");
+// }
+
+// Opdracht 4
+console.log()
+
+
+
+
+
